81fea087e588dd0d105beb5f11ec3acef329b0e3
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2018-09-03 00:31:52 +0700

    456

--- font/server/insert_money.php
+++ font/server/insert_money.php
@@ -6,26 +6,23 @@
         header("Location: ../index.php");
       }
     //give value from form_insert_paper.php
-    $paper_th = $_POST['paper_th'];
-    $paper_eng = $_POST['paper_eng'];
-    $abstract = $_POST['abstract'];
-    $keyword = $_POST['keyword'];
-    echo $paper_th,$paper_eng,$abstract,$keyword;
-    $ext = pathinfo(basename($_FILES["paper"]["name"]), PATHINFO_EXTENSION);
-    $new_taget_name = 'pdf_' . uniqid() . "." . $ext;
-    $target_path = "../uploads/";
+    $id_paper = $_GET['id'];
+    
+    $ext = pathinfo(basename($_FILES["money"]["name"]), PATHINFO_EXTENSION);
+    $new_taget_name = 'Bill_' . uniqid() . "." . $ext;
+    $target_path = "../../Bill/";
     $upload_path = $target_path . $new_taget_name;
     $uploadOk = 1;
 
     $imageFileType = strtolower(pathinfo($new_taget_name, PATHINFO_EXTENSION));
 
-    if ($_FILES["paper"]["size"] > 8000000) {
+    if ($_FILES["money"]["size"] > 8000000) {
         echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
 
     // Allow certain file formats
-    if ($imageFileType != "pdf") {
+    if ($imageFileType != "pdf"||$imageFileType != "jpg") {
         echo "Sorry, only PDF files are allowed.";
         $uploadOk = 0;
     }
@@ -36,35 +33,19 @@
     }
 
     else {
-        if (move_uploaded_file($_FILES["paper"]["tmp_name"], $upload_path)) {
+        if (move_uploaded_file($_FILES["money"]["tmp_name"], $upload_path)) {
             echo 'Move success.';
         }else {
             echo 'Move fail';
         }
     }
 
-    $paper = $_FILES["paper"]["name"];
+    $paper = $_FILES["money"]["name"];
     $b = $new_taget_name;
-    $a = "INSERT INTO paper (file_name,file_tmp_name,name_th,name_eng,abstract,key_word,status) 
-            VALUES ('$paper','$b','$paper_th','$paper_eng','$abstract','$keyword','5')";
+    $a = "UPDATE `paper` SET`money_status`='6',`tmp_money`='$b' WHERE paper_id = '$id_paper'";
             
     $r_a = mysqli_query($con,$a);
-    $c = "SELECT * FROM paper ORDER BY paper_id DESC LIMIT 1";
-    $result_c = mysqli_query($con, $c);
-    $row_c = mysqli_fetch_array($result_c);
-    $last_paper = $row_c['paper_id'];
-    
-    $r_p = "INSERT INTO `reviewer_paper`( `paper_id`) VALUES ('$last_paper')";
-    $r_r_p = mysqli_query($con,$r_p);
-
-    $id = $_SESSION['id'];
-
-    $p_u = "INSERT INTO `user_paper`( `paper_id`,username) VALUES ('$last_paper','$id')";
-    $r_p_u = mysqli_query($con,$p_u);
-
-    $r_a = "INSERT INTO `reviewer_answer`( `paper_id`) VALUES ('$last_paper')";
-    $r_r_a = mysqli_query($con,$r_a);
-    $r_r_a = mysqli_query($con,$r_a);
+  
     
     header("Location: ../user.php");
 
