9fed25016dfbeb51dee1cf0fb773e8f8d4d683e3
supakoa <41123305+Supakoa@users.noreply.github.com>
2018-09-03 23:58:39 +0700

    koa

--- font/modal/modal_money.php
+++ font/modal/modal_money.php
@@ -1,31 +1,29 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal<?php echo $row_money['paper_id'] ?>">
+<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modalmoney<?php echo $row_money['paper_id'] ?>">
   อัพโหลดหลักฐานการชำระเงิน
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="myModal<?php echo $row_money['paper_id'] ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="modalmoney<?php echo $row_money['paper_id'] ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-      <h4 class="modal-title" id="myModalLabel">รายละเอียด</h4>
+        <h4 class="modal-title" id="myModalLabel">รายละเอียด</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        
       </div>
       <div class="modal-body">
-      <div class="card">
-                           
-                            <div class="content">
-                                <?php
+        <div class="card">
+          <div class="content">
+          <?php
                                 $q_p = "SELECT * FROM paper WHERE paper_id = $id_paper";
-                                $r_q_p = mysqli_query($con,$q_p);
+                                $r_q_p = mysqli_query($con, $q_p);
                                 $row_q_p = mysqli_fetch_assoc($r_q_p);
                                 $status_paper = $row_q_p['status'];
                                 $q_status = "SELECT * FROM status_tb WHERE id = $status_paper";
                                 $result_status = mysqli_query($con, $q_status);
                                 $row_status = mysqli_fetch_array($result_status);
                                 ?>
-                                <div class="row">
+              <div class="row">
                                   <div class="col col-6-lg ms-auto">
                                 <h5 style="margin-left:10px">Paper ID : </h5>
                                   </div>
@@ -49,14 +47,14 @@
                                   </div>
                                   <div class="col col-6-lg ms-auto"><span><?php echo $row_q_p['abstract'] ?></span><br></div> 
                                 </div>
-                                <div class="row">
-                                
+
+ <div class="row">
                                 <?php
                                 $i = 1;
                                 $q_RA = "SELECT user.first_name,user.last_name,reviewer_answer.status,reviewer_answer.score,reviewer_answer.comment,status_tb.status
                                 FROM user,reviewer_answer,status_tb WHERE reviewer_answer.paper_id =$id_paper AND reviewer_answer.reviewer_id = user.username AND status_tb.id = reviewer_answer.status";
                                 $result_RA = mysqli_query($con, $q_RA);
-                                
+
                                 while ($row_RA = mysqli_fetch_array($result_RA)) { ?>
                                   
                                   <div class="col col-12-lg ms-auto">
@@ -67,35 +65,23 @@
                                     <p style="margin-left:10px"> คอมเมนต์ : <?php echo $row_RA['comment'] ?></p>
                                     </div>
                                 <?php 
-                            
-                            } ?>
-                              <div class="col-lg-6">
-                              <form action="">
+
+                              } ?>
+                              </div>
+
+          <div>
+                            <form action="">
                                 <input type="file">
-                              
+                                <button type="submit" class="btn btn-primary btn-md" >Submit</button>
                               </form>
-                              </div>
-                              </div>
-                                <div>
-                                <!-- ใส่อินพุชชนิดไฟล์ ไว้รับสลิป -->
-                                </div>
-                                <br>
-                                <div style="text-align:right" >
-                                <button type="submit" class="btn btn-primary btn-sm" id="sendMessageButton">Submit</button>
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                </div><br>
-                            </div>
-                                </div>
-                                
-                                
-                                
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div> 
+                             
+        </div>
+        <br>
+          <div style="text-align:right;margin-right:15px" >
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div><br>                     
       </div>
-     
-    </div>
+    </div>   
   </div>
 </div>
+     
