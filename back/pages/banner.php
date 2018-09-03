8ceac214763ef5bd72bba076092c020b97dab829
supakoa <41123305+Supakoa@users.noreply.github.com>
2018-09-04 00:30:46 +0700

    koa

--- back/pages/banner.php
+++ back/pages/banner.php
@@ -67,63 +67,47 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Banner</h1>
-                    <h5 style="text-align:center">Paper File</h5><br>
-            	<div class="row">
-                    <div class="col col-8-lg">
-                    <form action = "server/insert_banner.php" method ="POST" enctype="multipart/form-data">
-                    <div class="control-group">
-                    <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <h1 class="page-header">Banner</h1>
+                            <h5 style="text-align:center">Paper File</h5><br>
+                            <form action = "server/insert_banner.php" method ="POST" enctype="multipart/form-data">
                         <div class="row">
-                            <div class="col col-lg-6">
-                        <input class="form-control" name="banner" type="file" placeholder="File" required="required">
+                            <div class=" col-lg-6">
+                            <input class="form-control" name="banner" type="file" placeholder="File" required="required">
                             </div>
-                            <div class="col col-lg-6" >
-                        <button type="submit" class="btn btn-success" >อัพโหลด</button>
+                            <div class=" col-lg-6" >
+                                <button type="submit" class="btn btn-success" >อัพโหลด</button>
                             </div>
                         </div>
-                        </form> 
-                        
-                    </div>
-                </div>
-                <div class="modal fade" id="submit_modal_banner" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog modal-sm" role="document">
-                                      
-                                            <div class="modal-content">
-                                            <div class="modal-header" style="text-align:center" >
-                                                <h3 class="modal-title" id="exampleModalLabel">ยืนยัน</h3>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body" style="text-align:center" >
-                                            
-                                            </div>
-                                            <div class="modal-footer" style="text-align:center">
-                                           <a href="server/insert_banner.php?id=1"><button type="submit" class="btn btn-success" > ยืนยัน</button></a> 
-                                                    <button type="button" class="btn btn-danger " data-dismiss="modal">ยกเลิก</button>
-                                            </div>
-                                            </div>
-                                           
-                                        </div>
-                                        </div>
-                                        <div class="container-fluid">
-                                        <div class="jumbotron" >
-                                            <div  class="row" >
-                                            <div class="col col-lg-12">
-                                            <div style="text-align:center">
-                                            <img src="banner/<?php echo $_SESSION['tmp_banner']?>" alt="banner">
-                                            <div class="container" style="text-align:center;">
-                            <br>
-                            <a href="" class="btn btn-info btn-md " data-toggle="modal" data-target="#submit_modal_banner">Update Banner</a>
-                        </div>
-                                            </div>
-                                            </div>
-                                            </div>
-                                            </div>
-                                        </div>
+                                <br></form> 
                 </div>
             </div>
-           
+                
+                                    <div class="container-fluid ">
+                                        <div class="jumbotron" >
+                                            <div style="text-align:left">
+                                                <img src="banner/<?php echo $_SESSION['tmp_banner']?>" alt="banner">
+                                            </div>
+                                        </div>
+                                        <div class="container" style="text-align:center;">
+                                                    <br><button  class="btn btn-info btn-md " data-toggle="modal" data-target="#submit_modal_banner">Update Banner</button>
 
+                                        <div class="modal fade" id="submit_modal_banner" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog modal-sm" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header" style="text-align:center" >
+                                                        <h3 class="modal-title" id="exampleModalLabel">ยืนยัน</h3>
+                                                    </div>
+                                                    <div class="modal-footer" style="text-align:center">
+                                                        <a href="server/insert_banner.php?id=1"><button type="submit" class="btn btn-success" > ยืนยัน</button></a> 
+                                                        <button type="button" class="btn btn-danger " data-dismiss="modal">ยกเลิก</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                            </div><br>
+                                    </div>
+                
+            </div>
     </div>
     
 
